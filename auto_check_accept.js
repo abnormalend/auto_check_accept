209faf5aeb0ae2f658412ff241c44f14bc746e18
abnormalend <brentarogers@gmail.com>
2019-02-01 16:26:33 -0500

    add licence, update and download url

--- auto_check_accept.js
+++ auto_check_accept.js
@@ -1,11 +1,14 @@
 // ==UserScript==
 // @name         Auto Check Accept on Steam
 // @namespace    https://github.com/abnormalend
+// @license      MIT
 // @version      0.1
 // @description  Automatically add games to your library when clicking a register link for steam.
 // @author       Brent Rogers
 // @match        https://store.steampowered.com/account/registerkey*
 // @grant        none
+// @updateURL    https://raw.githubusercontent.com/abnormalend/auto_check_accept/master/auto_check_accept.js
+// @downloadURL  https://raw.githubusercontent.com/abnormalend/auto_check_accept/master/auto_check_accept.js
 // ==/UserScript==
 
 (function() {
